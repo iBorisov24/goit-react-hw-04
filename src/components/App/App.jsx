a95a268ec977a7683edb59fe0c3596d45b636f1a
Avstrer <kot3kote2409@gmail.com>
2024-05-19 21:11:48 +0300

    .

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,81 +1,83 @@
-import { useState, useEffect } from "react";
-import { fetchUrl } from "./fetchUrl";
-import SearchBar from "../SearchBar/SearchBar";
-import Loader from "../Loader/Loader";
-import ImageGallery from "../ImageGallery/ImageGallery";
-import ErrorMesage from "../ErrorMessage/ErrorMessage";
-import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
-import toast, { Toaster } from "react-hot-toast";
-import ImageModal from "../ImageModal/ImageModal";
+import { useState, useEffect } from 'react';
+import { fetchUrl } from './fetchUrl';
+import SearchBar from '../SearchBar/SearchBar';
+import Loader from '../Loader/Loader';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import ErrorMesage from '../ErrorMessage/ErrorMessage';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import toast, { Toaster } from 'react-hot-toast';
+import ImageModal from '../ImageModal/ImageModal';
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [collection, setCollection] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loader, setLoader] = useState(false);
-  const [error, setError] = useState(false);
-  const [isOpen, setisOpen] = useState(false);
-  const [imageInfo, setimageInfo] = useState("");
+	const [query, setQuery] = useState('');
+	const [collection, setCollection] = useState([]);
+	const [page, setPage] = useState(1);
+	const [loader, setLoader] = useState(false);
+	const [error, setError] = useState(false);
+	const [isOpen, setisOpen] = useState(false);
+	const [imageInfo, setimageInfo] = useState('');
 
-  useEffect(() => {
-    const request = async () => {
-      try {
-        setError(false);
-        setLoader(true);
-        const result = await fetchUrl(query, page);
-        if (page > result.total_pages) {
-          toast.error("Sorry, it is last page");
-        }
-        result.results.length !== 0
-          ? setCollection((prevCollection) => [
-              ...prevCollection,
-              ...result.results,
-            ])
-          : toast.error("Nothing to this query not finded, sorry", {
-              position: "top-right",
-            });
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoader(false);
-      }
-    };
+	useEffect(() => {
+		const request = async () => {
+			try {
+				setError(false);
+				setLoader(true);
+				const result = await fetchUrl(query, page);
 
-    if (query !== "") request();
-  }, [query, page]);
+				if (page > result.total_pages && collection.length > 1) {
+					toast.error('Sorry, it is last page');
+				}
+				result.results.length !== 0
+					? setCollection(prevCollection => [
+							...prevCollection,
+							...result.results,
+					  ])
+					: toast.error('Nothing to this query not finded, sorry', {
+							position: 'top-right',
+					  });
+			} catch (error) {
+				setError(true);
+			} finally {
+				setLoader(false);
+			}
+		};
 
-  const openModal = (url) => {
-    setisOpen(true);
-    setimageInfo(url);
-  };
+		if (query !== '') request();
+	}, [query, page]);
 
-  const closeModal = () => {
-    setisOpen(false);
-    setimageInfo("");
-  };
+	const openModal = url => {
+		setisOpen(true);
+		setimageInfo(url);
+	};
 
-  const onSubmit = (value) => {
-    setCollection([]);
-    setQuery(value);
-  };
+	const closeModal = () => {
+		setisOpen(false);
+		setimageInfo('');
+	};
 
-  const onLoadMore = () => setPage(page + 1);
+	const onSubmit = value => {
+		setCollection([]);
+		setPage(1);
+		setQuery(value);
+	};
 
-  return (
-    <>
-      <SearchBar onSubmit={onSubmit} />
-      <ImageGallery gallery={collection} onClickFoo={openModal} />
-      <ImageModal
-        openModal={openModal}
-        closeModal={closeModal}
-        imageInfo={imageInfo}
-        isOpen={isOpen}
-      />
-      {loader && <Loader />}
-      {error && <ErrorMesage />}
-      {collection.length > 0 && <LoadMoreBtn clickBtn={onLoadMore} />}
-    </>
-  );
+	const onLoadMore = () => setPage(page + 1);
+
+	return (
+		<>
+			<SearchBar onSubmit={onSubmit} />
+			<ImageGallery gallery={collection} onClickFoo={openModal} />
+			<ImageModal
+				openModal={openModal}
+				closeModal={closeModal}
+				imageInfo={imageInfo}
+				isOpen={isOpen}
+			/>
+			{loader && <Loader />}
+			{error && <ErrorMesage />}
+			{collection.length > 0 && <LoadMoreBtn clickBtn={onLoadMore} />}
+		</>
+	);
 }
 
 export default App;
